35180e045f46b73bf408af21c916db2323a8e472
Rogerio Ribeiro <rogerioribeiro@rdtechfitness.com.br>
2023-05-26 11:52:56 -0300

    Inserçao de usuario com POST

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.repository.UserRepositoy;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
