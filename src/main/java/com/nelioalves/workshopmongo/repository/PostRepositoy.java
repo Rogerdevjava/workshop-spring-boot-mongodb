5c5035aff37328f988aef38a4f7560ec4c75250b
Rogerio Ribeiro <rogerioribeiro@rdtechfitness.com.br>
2023-05-27 15:20:55 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepositoy.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepositoy.java
@@ -1,5 +1,7 @@
 package com.nelioalves.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,9 @@ import com.nelioalves.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepositoy extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
+
+
+
